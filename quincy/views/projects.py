7cf100393e9fbf18cef1d45234066603ae6748a7
Corrin Meyer <corrin.meyer@gmail.com>
2012-04-07 00:25:33 -0400

    Added initial project view templates.

--- quincy/views/projects.py
+++ quincy/views/projects.py
@@ -1,12 +1,41 @@
 from flask import Blueprint
-from flask import current_app
+from flask import current_app, render_template
 
 
 projects = Blueprint('projects', __name__)
 
 
-@projects.route('/')
+@projects.route('/', methods=['GET'])
 def index():
-    print current_app.db
-    return 'Projects - Index'
+    return render_template('projects/index.html')
+
+
+@projects.route('/new', methods=['GET'])
+def new():
+    return render_template('projects/new.html')
+
+
+#@projects.route('/<name>')
+#def show():
+#    return render_template('projects/show.html')
+#
+#
+#@projects.route('/<name>/edit')
+#def edit():
+#    return render_template('projects/edit.html')
+#
+#
+#@projects.route('/', methods=['POST'])
+#def create():
+#    pass
+#
+#
+#@projects.route('/<name>/update', methods=['POST'])
+#def update():
+#    pass
+#
+#
+#@projects.route('/<name>/delete', methods=['POST'])
+#def destroy():
+#    pass
 
--- test/views/test_projects.py
+++ test/views/test_projects.py
@@ -1,3 +1,11 @@
+#! /usr/bin/env python
+
+def setup_function(fn):
+    pass
+
+def teardown_function(fn):
+    pass
+
 def test1():
     pass
 
